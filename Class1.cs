95c4b904ff240426721b28c6f8c395010ba24c1a
Ian Hooey <ian.hooey@gmail.com>
2023-09-26 22:17:57 -0500

    Updated

--- Class1.cs
+++ Class1.cs
@@ -3,6 +3,40 @@ using UnityEngine;
 
 namespace HFBlade
 {
+    public class HFBladeConfig : ThunderScript
+    {
+        [ModOption(name: "Reduced Sharpness", tooltip: "Reduces the sharpness of all HF Blades", valueSourceName: nameof(booleanOption), defaultValueIndex = 1)]
+        public static void reducedSharpness(bool value)
+        {
+            float damper = value ? 5000 : 0;
+            Catalog.GetData<DamagerData>("HFBladeSlash").penetrationDamper = damper;
+            Catalog.GetData<DamagerData>("HFBladePierce").penetrationDamper = damper;
+            foreach (Item item in Item.allActive)
+            {
+                foreach (Damager damager in item.GetComponentsInChildren<Damager>())
+                {
+                    if (damager.data.id == "HFBladeSlash" || damager.data.id == "HFBladePierce") damager.data.penetrationDamper = damper;
+                }
+            }
+        }
+        [ModOption(name: "Fix Stuck HF Blades", tooltip: "Stops all HF Blades from piercing or slashing what they're currently piercing/slashing", valueSourceName: nameof(booleanOption), defaultValueIndex = 1)]
+        public static void unpenetrateAll(bool value)
+        {
+            if(value)
+            foreach (Item item in Item.allActive)
+            {
+                foreach (Damager damager in item.GetComponentsInChildren<Damager>())
+                {
+                    if (damager.data.id == "HFBladeSlash" || damager.data.id == "HFBladePierce") damager.UnPenetrateAll();
+                }
+            }
+        }
+        public static ModOptionBool[] booleanOption =
+        {
+            new ModOptionBool("Enabled", true),
+            new ModOptionBool("Disabled", false)
+        };
+    }
     public class MurasamaModule : ItemModule
     {
         public float Speed;
@@ -81,10 +115,10 @@ namespace HFBlade
                 sword.IgnoreObjectCollision(item);
                 sword.IgnoreRagdollCollision(ragdollHand.ragdoll);
                 holder.UnSnap(sword);
-                sword.rb.AddForce(-sword.holderPoint.forward * Speed, ForceMode.Impulse);
+                sword.physicBody.AddForce(-sword.holderPoint.forward * Speed, ForceMode.Impulse);
                 sword.isThrowed = true;
                 sword.SetColliderAndMeshLayer(GameManager.GetLayer(LayerName.MovingItem));
-                sword.rb.collisionDetectionMode = Catalog.gameData.collisionDetection.throwed;
+                sword.physicBody.collisionDetectionMode = Catalog.gameData.collisionDetection.throwed;
                 if (!Item.allThrowed.Contains(sword)) Item.allThrowed.Add(sword);
             }
         }
